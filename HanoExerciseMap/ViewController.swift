90842661138751e702e34f4a13225f0bdbe2f524
Glsme <glasses.str.man@gmail.com>
2022-08-11 15:52:02 +0900

    Feat: 위치 권한 설정 추가

--- HanoExerciseMap/ViewController.swift
+++ HanoExerciseMap/ViewController.swift
@@ -19,12 +19,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         locationManager.delegate = self
+        checkUserDeviceLocationServiceAuthorization()
         
-        setRegionAndAnnotation()
+        let center = CLLocationCoordinate2D(latitude: 37.621068, longitude:  127.041060)
+        setRegionAndAnnotation(center: center)
     }
     
-    func setRegionAndAnnotation() {
-        let center = CLLocationCoordinate2D(latitude: 37.621068, longitude:  127.041060)
+    func setRegionAndAnnotation(center: CLLocationCoordinate2D) {
+        
         let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
         mapView.setRegion(region, animated: true)
         
@@ -38,8 +40,55 @@ class ViewController: UIViewController {
 
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(#function)
+        
         if let coordinate = locations.last?.coordinate {
-            
+            setRegionAndAnnotation(center: coordinate)
+        }
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function)
+        checkUserDeviceLocationServiceAuthorization()
+    }
+}
+
+extension ViewController {
+    func checkUserDeviceLocationServiceAuthorization() {
+        let authorizationStatus: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            authorizationStatus = locationManager.authorizationStatus
+        } else {
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            checkUserCurrentLocationAuthorization(authorizationStatus)
+        } else {
+            print("위치 서비스가 꺼져 있어 위치 권한 요청을 하지 못합니다.")
+        }
+    }
+    
+    func checkUserCurrentLocationAuthorization(_ authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .notDetermined:
+            print("NOT DETERMINED")
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            print("DENIED, 아이폰 설정으로 유도")
+        case .authorizedWhenInUse:
+            print("WHEN IN USE")
+            locationManager.startUpdatingLocation()
+        default:
+            print("DEFUALT")
         }
     }
 }

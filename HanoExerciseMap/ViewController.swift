7a679a6752b373922a27274c976f8ce14d0d5d38
Glsme <glasses.str.man@gmail.com>
2022-08-11 15:55:57 +0900

    Feat: 위치 권한 거부 설정 시 Alert 출력

--- HanoExerciseMap/ViewController.swift
+++ HanoExerciseMap/ViewController.swift
@@ -25,6 +25,12 @@ class ViewController: UIViewController {
         setRegionAndAnnotation(center: center)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        showRequestLocationServiceAlert()
+    }
+    
     func setRegionAndAnnotation(center: CLLocationCoordinate2D) {
         
         let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
@@ -90,5 +96,20 @@ extension ViewController {
         default:
             print("DEFUALT")
         }
+    }
+    
+    func showRequestLocationServiceAlert() {
+      let requestLocationServiceAlert = UIAlertController(title: "위치정보 이용", message: "위치 서비스를 사용할 수 없습니다. 기기의 '설정>개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+        
+      let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+          if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+              UIApplication.shared.open(appSetting)
+          }
+      }
+      let cancel = UIAlertAction(title: "취소", style: .default)
+      requestLocationServiceAlert.addAction(cancel)
+      requestLocationServiceAlert.addAction(goSetting)
+      
+      present(requestLocationServiceAlert, animated: true, completion: nil)
     }
 }
